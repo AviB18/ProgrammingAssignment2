d9a232f4a490d6f5cfed3d844125f4261de59a67
Avinash Bhat <avinash_march1988@yahoo.co.in>
2014-10-26 07:14:58 -0700

    Programming assignment
    This is assignment on caching of data for faster computation.
    In this example, we use matrix inverse for understanding this concept.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script defines two wrappers for dealing with "special matrix".
+## In this example, we cache the inverse of this matrix for further usage.
 
-## Write a short comment describing this function
-
+## This function provides an interface for dealing with the "special matrix".
+## It has interfaces like 
+##     setting the matrix
+##     getting the matrix
+##     setting the inverse of matrix for caching
+##     getting the cached inverse of matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    
+    setMatrix <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    
+    getMatrix <- function() {
+        x
+    }
+    
+    setInverse <- function(inverse) {
+        inv <<- inverse
+    }
+    
+    getInverse <- function() {
+        inv
+    }
+    
+    list(setMatrix = setMatrix, getMatrix = getMatrix,
+         setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## This function is a wrapper for finding inverse of "special matrix".
+## If the inverse of "special matrix" is already computed, get the cached
+## value through its interface. Else, newly compute the inverse and store
+## it via its interface.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    
+    if(!is.null(inv)) {
+        message("getting inverse from cache")
+        return (inv)
+    }
+    
+    mat <- x$getMatrix()
+    inv <- solve(mat)
+    x$setInverse(inv)
+    
+    inv
 }
